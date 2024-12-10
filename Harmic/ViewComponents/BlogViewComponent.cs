950ea2cb3465d9471bc6a5d1a8ef99cc41e1ee92
bon-03 <hungbumda2003@gmail.com>
2024-12-10 20:47:25 +0700

    th5

--- Harmic/ViewComponents/BlogViewComponent.cs
+++ Harmic/ViewComponents/BlogViewComponent.cs
@@ -15,11 +15,11 @@ namespace Harmic.ViewComponents
 
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            var items = _context.TbBlogs.Include(m => m.CategoryBlog)
-                                           .Where(m => (bool)m.IsActive)
-                                           .Where(m => m.IsNew);
+            var items = _context.TbBlogs.Include(m => m.Category)
+                                           .Where(m => (bool)m.IsActive);
+                                           
             return await Task.FromResult<IViewComponentResult>(
-                View(items.OrderByDescending(m => m.ProductId).ToList()));
+                View(items.OrderByDescending(m => m.BlogId).ToList()));
         }
     }
 }
